9fa228668e43c68823d8437c9e6ee530b9c8f8de
AnjiMuduthanapally <105631021+AnjiMuduthanapally@users.noreply.github.com>
2022-05-20 12:03:04 +0530

    Add files via upload

---
+++ using_dependon/main.tf
@@ -0,0 +1,32 @@
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+resource "azurerm_resource_group" "main" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+data "azurerm_resource_group" "main" {
+  name = "data_soruce-rg"
+  depends_on = [
+    azurerm_resource_group.main
+  ]
+
+}
+resource "azurerm_storage_account" "example" {
+  name                     = "mycheckstorage"
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = data.azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+
+  tags = {
+    environment = "staging"
+  }
+}
+
---
+++ using_dependon/vars.tf
@@ -0,0 +1,23 @@
+variable "rg_name" {
+  type    = string
+  default = "data_soruce-rg"
+}
+
+variable "rg_location" {
+  type    = string
+  default = "west europe"
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+variable "client_secret" {
+  type = string
+}
+variable "tenant_id" {
+  type = string
+}
