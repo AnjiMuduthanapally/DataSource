57dc814b5f81cc4b0a4d9b555cf3a1f297931586
AnjiMuduthanapally <105631021+AnjiMuduthanapally@users.noreply.github.com>
2022-05-20 11:44:33 +0530

    Update main.tf

--- creating_rg/main.tf
+++ creating_rg/main.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
     features {
       
     }
-  subscription_id = "3e68b387-c893-46d3-8c09-855b307d65a4"
-  client_id       = "dc9bf6c5-0cc8-4bb2-979e-43b75f6d8398"
-  client_secret   = "Ag88Q~dTLNGnge7sfg6EWtLLTEcGKv6KHtZKIddY"
-  tenant_id       = "d08a11c9-264f-4371-82d4-b20e9d8257b9"
+  subscription_id = var.subscription_id
+  client_id       = var.varclient_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   
 }
 
