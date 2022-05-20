b42c830f3d45f824191b9905b3163b5ebb8aacfa
AnjiMuduthanapally <105631021+AnjiMuduthanapally@users.noreply.github.com>
2022-05-20 11:42:23 +0530

    Add files via upload

--- creating_rg/main.tf
+++ creating_rg/main.tf
@@ -1,1 +1,15 @@
+provider "azurerm" {
+    features {
+      
+    }
+  subscription_id = "3e68b387-c893-46d3-8c09-855b307d65a4"
+  client_id       = "dc9bf6c5-0cc8-4bb2-979e-43b75f6d8398"
+  client_secret   = "Ag88Q~dTLNGnge7sfg6EWtLLTEcGKv6KHtZKIddY"
+  tenant_id       = "d08a11c9-264f-4371-82d4-b20e9d8257b9"
+  
+}
 
+resource "azurerm_resource_group" "main" {
+  name = var.rg_name
+  location = var.rg_location
+}
---
+++ creating_rg/vars.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+  type = string
+  default = "data_soruce-rg"
+}
+
+variable "rg_location" {
+  type = string
+  default = "west europe"
+}
+
